3d095d66834afbdf0c9dff30f70984b67c47f7e9
Damián López <lopezrunco@gmail.com>
2023-09-12 12:12:54 +0530

    site metadata, fetching styles dynamically

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+
+function starterwptheme_register_styles()
+{
+
+    $version = wp_get_theme()->get('Version');
+    wp_enqueue_style('starterwptheme-style', get_template_directory_uri() . "/style.css", array('starterwptheme-bootstrap'), $version, 'all');
+    wp_enqueue_style('starterwptheme-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '4.4.1', 'all');
+    wp_enqueue_style('starterwptheme-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '5.13.0', 'all');
+
+}
+
+add_action('wp_enqueue_scripts', 'starterwptheme_register_styles');
+
+?>
