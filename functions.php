c08ed973c09058090cc9f9fee68a6131e807a0b5
Damián López <lopezrunco@gmail.com>
2023-09-12 16:57:07 +0530

    dynamic single post & post-thumbnails theme support

--- functions.php
+++ functions.php
@@ -2,9 +2,9 @@
 
 function starterwptheme_theme_support()
 {
-    // Adds dynamic title tag support
     add_theme_support('title-tag');
     add_theme_support('custom-logo');
+    add_theme_support('post-thumbnails');
 }
 
 add_action('after_setup_theme', 'starterwptheme_theme_support');
