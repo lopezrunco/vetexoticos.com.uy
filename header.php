2ca9d5a02ffe049b6fc7d476967b28f511a349d5
membrillo amargo <lopezrunco@gmail.com>
2024-06-05 13:47:08 +0530

    feat: Add Bootstrap, SCSS styles and My Account and Cart PHP code - Add Bootstrap CDNs - Add SCSS to form, general, nav, top, variables - Add PHP functions to show My Account and Cart modules in the Top section

--- header.php
+++ header.php
@@ -11,6 +11,8 @@
     <link rel="shortcut icon" href="/wp-content/themes/vetexoticos/assets/images/logo.png">
     <!-- FontAwesome CDN -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Bootstrap CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     <?php
     wp_head();
@@ -19,42 +21,53 @@
 </head>
 
 <body>
+    <header>
+        <div class="top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-6">
+                        <small><i class="fa-solid fa-location-dot me-1"></i> Montevideo, Uruguay</small>
+                    </div>
+                    <div class="col-6">
+                        <div class="account-options">
+                            <a href="<?php echo get_permalink(get_option('woocommerce_myaccount_page_id')); ?>">
+                                <i class="fas fa-user"></i>
+                            </a>
+                            <a href="<?php echo wc_get_cart_url(); ?>">
+                                <i class="fas fa-shopping-cart"></i>
+                                <span class="badge bg-secondary"><?php echo WC()->cart->get_cart_contents_count(); ?></span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-    <header class="header text-center">
-        <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-dark">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
-                <i class="fa-solid fa-bars menu-icon"></i>
-            </button>
-
-            <div id="navigation" class="collapse navbar-collapse flex-lg-row flex-column">
-
-                <?php
-                if (function_exists('the_custom_logo')) {
-                    $custom_logo_id = get_theme_mod('custom_logo');
-                    $logo = wp_get_attachment_image_src($custom_logo_id);
-                }
-                ?>
-
-                <a href="<?php echo esc_url(home_url('/')); ?>">
-                    <img class="logo" src="<?php echo $logo[0] ?>" alt="logo">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary py-lg-0">
+            <div class="container">
+                <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>">
+                    <img class="main-logo" src="https://vetexoticos.uy/wp-content/uploads/2024/06/logo.png" alt="Logo Vetexoticos">
                 </a>
-
-                <?php
-                wp_nav_menu(
-                    array(
-                        'menu' => 'primary',
-                        'container' => '',
-                        'theme_location' => 'primary',
-                        'items_wrap' => '<ul id="" class="navbar-nav flex-lg-row flex-column text-sm-center text-md-left">%3$s</ul>'
-                    )
-                );
-                ?>
-
+                <button class="navbar-toggler navbar-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse py-3" id="navbarSupportedContent">
+                    <?php
+                    wp_nav_menu(
+                        array(
+                            'menu' => 'primary',
+                            'container' => '',
+                            'theme_location' => 'primary',
+                            'items_wrap' => '<ul class="navbar-nav me-auto mb-2 mb-lg-0">%3$s</ul>'
+                        )
+                    );
+                    ?>
+                    <?php get_search_form(); ?>
+                </div>
             </div>
         </nav>
-        </div>
     </header>
+
     <div class="main-wrapper">
         <?php if (!is_front_page()) : ?>
             <header class="page-title theme-bg-light text-center gradient py-3">
