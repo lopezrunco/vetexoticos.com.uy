438ca74e526127eeacbd923a9ab35dfa4f98a289
membrillo amargo <lopezrunco@gmail.com>
2024-06-06 12:29:28 +0530

    refactor: Abstract product-grid section to reuse

--- front-page.php
+++ front-page.php
@@ -55,7 +55,7 @@ get_header();
 	</div>
 </div>
 
-<section class="latest-products">
+<section class="product-grid">
 	<article class="container">
 		<?php
 		$latest_products = wc_get_products(
@@ -78,15 +78,17 @@ get_header();
 				</div>
 			</div>
 		';
-			echo '<div class="latest-products-container">';
+			echo '<div class="row">';
 			foreach ($latest_products as $product) {
 				?>
-				<div class="product-wrapper">
-					<a href="<?php echo esc_url($product->get_permalink()); ?>">
-						<?php echo $product->get_image(); ?>
-						<h5><?php echo $product->get_name(); ?></h5>
-						<span class="price"><?php echo $product->get_price_html(); ?></span>
-					</a>
+				<div class="col-lg-4 mb-4">
+					<div class="product-wrapper">
+						<a href="<?php echo esc_url($product->get_permalink()); ?>">
+							<?php echo $product->get_image(); ?>
+							<h5><?php echo $product->get_name(); ?></h5>
+							<span class="price"><?php echo $product->get_price_html(); ?></span>
+						</a>
+					</div>
 				</div>
 				<?php
 			}
@@ -206,6 +208,55 @@ get_header();
 				</div>
 			</div>
 		</div>
+	</article>
+</section>
+
+<section class="product-grid">
+	<article class="container">
+		<?php
+		$category_slug = 'Juguetes';
+		$category = get_term_by('slug', $category_slug, 'product_cat');
+
+		$latest_products = wc_get_products(
+			array(
+				'limit' => 4,
+				'orderby' => 'date',
+				'order' => 'DESC',
+				'category' => array($category_slug),
+			)
+		);
+		if ($latest_products) {
+			$category_link = get_term_link($category->term_id, 'product_cat');
+			echo '
+			<div class="section-title-more">
+				<div>
+					<span class="top-title">Esencial para tus mascotas</span>
+					<h2>' . esc_html($category->name) . '</h2>
+				</div>
+				<div>
+					<a class="btn btn-primary" href="' . esc_url($category_link) . '">Ver más ' . esc_html($category->name) . ' <i class="fa-solid fa-chevron-right"></i></a>
+				</div>
+			</div>
+		';
+			echo '<div class="row">';
+			foreach ($latest_products as $product) {
+				?>
+				<div class="col-lg-3 mb-4">
+					<div class="product-wrapper">
+						<a href="<?php echo esc_url($product->get_permalink()); ?>">
+							<?php echo $product->get_image(); ?>
+							<h5><?php echo $product->get_name(); ?></h5>
+							<span class="price"><?php echo $product->get_price_html(); ?></span>
+						</a>
+					</div>
+				</div>
+				<?php
+			}
+			echo '</div>';
+		} else {
+			echo '<p>No se encontraron productos en la categoría ' . $category_slug . '</p>';
+		}
+		?>
 	</article>
 </section>
 
