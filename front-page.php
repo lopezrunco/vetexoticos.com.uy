13c9e9aaaae16248f562e6dd90bc88155432dd75
membrillo amargo <lopezrunco@gmail.com>
2024-06-06 15:44:35 +0530

    feat: Add featured services section.

--- front-page.php
+++ front-page.php
@@ -362,6 +362,31 @@ get_header();
 	</article>
 </section>
 
+<section class="featured-services bg-primary">
+	<div class="paws-overlay">
+		<article class="container">
+			<div class="row">
+				<div class="col-lg-3 mb-4">
+					<i class="fa-solid fa-truck"></i>
+					<p>Envíos gratuitos en Montevideo</p>
+				</div>
+				<div class="col-lg-3 mb-4">
+					<i class="fa-regular fa-credit-card"></i>
+					<p>Métodos de pago amigables</p>
+				</div>
+				<div class="col-lg-3 mb-4">
+					<i class="fa-solid fa-truck"></i>
+					<p>Descuentos al por mayor</p>
+				</div>
+				<div class="col-lg-3 mb-4">
+					<i class="fa-regular fa-comments"></i>
+					<p>Asistencia post venta</p>
+				</div>
+			</div>
+		</article>
+	</div>
+</section>
+
 <section class="news-grid">
 	<article class="container">
 		<?php
@@ -404,7 +429,8 @@ get_header();
 							<div class="post-excerpt">
 								<?php echo wp_trim_words(get_the_excerpt(), 30, '...'); ?>
 							</div>
-							<a href="<?php the_permalink(); ?>" class="btn btn-primary read-more">Continuar leyendo <i class="fa-solid fa-chevron-right"></i></a>
+							<a href="<?php the_permalink(); ?>" class="btn btn-primary read-more">Continuar leyendo <i
+									class="fa-solid fa-chevron-right"></i></a>
 						</div>
 					</div>
 				</div>
