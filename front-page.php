e217d5240c8a29a111cf55d64ca3b66567638e7c
membrillo amargo <lopezrunco@gmail.com>
2024-06-06 12:57:15 +0530

    feat: Add testimonials section.

--- front-page.php
+++ front-page.php
@@ -260,6 +260,56 @@ get_header();
 	</article>
 </section>
 
+<section class="testimonials bg-primary">
+	<div class="paws-overlay">
+		<div class="container">
+			<div class="row">
+				<div class="col mb-5">
+					<h2>Testimonios de clientes</h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-4 mb-5">
+					<div class="content-wrapper">
+						<div class="image-wrapper">
+							<img src="<?php echo get_template_directory_uri(); ?>/assets/images/client-1.jpg"
+								alt="Foto de cliente">
+						</div>
+						<p class="testimonial">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis scelerisque, nisi sed sollicitudin interdum, nisl est bibendum massa, at cursus magna urna ut odio. 
+						</p>
+						<span class="client-name">Dino Capelli</span>
+					</div>
+				</div>
+				<div class="col-lg-4 mb-5">
+					<div class="content-wrapper">
+						<div class="image-wrapper">
+							<img src="<?php echo get_template_directory_uri(); ?>/assets/images/client-2.jpg"
+								alt="Foto de cliente">
+						</div>
+						<p class="testimonial">
+							Vivamus dictum, sit amet orci tortor at placerat bibendum, erat lacus laoreet nulla, id egestas metus purus sit amet orci. Donec fringilla hendrerit convallis.
+						</p>
+						<span class="client-name">Mario Perazza</span>
+					</div>
+				</div>
+				<div class="col-lg-4 mb-5">
+					<div class="content-wrapper">
+						<div class="image-wrapper">
+							<img src="<?php echo get_template_directory_uri(); ?>/assets/images/client-3.jpg"
+								alt="Foto de cliente">
+						</div>
+						<p class="testimonial">
+							Auctor est habitasse amet nunc, interdum vel mattis sodales cras. Vivamus dictum, tortor at placerat bibendum, erat lacus laoreet nulla, id egestas metus purus sit amet orci.
+						</p>
+						<span class="client-name">Carlos Fontana</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
 <section class="product-grid bg-light">
 	<article class="container">
 		<?php
