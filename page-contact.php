5038e57395ff2029bbe5da528c11403d9ba59ff3
membrillo amargo <lopezrunco@gmail.com>
2024-06-07 17:02:16 +0530

    feat: Add map to contact page & fix spacing in desktop version.

--- page-contact.php
+++ page-contact.php
@@ -6,7 +6,7 @@ get_header();
 <section>
     <article class="container">
         <div class="row">
-            <div class="col-lg-8 mb-5">
+            <div class="col-lg-8 mb-0 mb-lg-5">
                 <?php echo do_shortcode('[contact-form-7 id="39d3242" title="Contact form 1"]'); ?>
             </div>
             <div class="col-lg-4">
@@ -28,6 +28,10 @@ get_header();
         </div>
     </article>
 </section>
+
+<div class="bg-primary">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d25517.552489363698!2d-56.166678735456934!3d-34.89036941498789!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x959f80ffc63bf7d3%3A0x6b321b2e355bec99!2sMontevideo%2C%20Departamento%20de%20Montevideo%2C%20Uruguay!5e0!3m2!1ses-419!2sin!4v1717759775551!5m2!1ses-419!2sin" width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
 
 <?php
 get_footer();
