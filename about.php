f1bd5817a833d4eb6e5165c366395cc19918e6ab
membrillo amargo <lopezrunco@gmail.com>
2024-06-07 11:27:55 +0530

    feat: Add staff grid section.

--- about.php
+++ about.php
@@ -46,19 +46,84 @@ get_header(); ?>
 </section>
 
 <section class="call-to-action">
-	<article class="gray-overlay">
-		<article class="container">
-			<div class="content-wrapper">
-				<h6>Ofrecemos una amplia gama de productospara perros, gatos, aves, peces y más.</h6>
-				<h1>Descubre todo lo que necesitas para cuidar y consentir a tu mascota.</h1>
-				<?php
-				$shop_page = get_page_by_path('shop');
-				$shop_page_url = get_permalink($shop_page->ID);
-				echo '<a class="btn btn-light" href="' . esc_url($shop_page_url) . '">Ir a la tienda <i class="fa-solid fa-store"></i></a>';
-				?>
-			</div>
-		</article>
-	</article>
+    <article class="gray-overlay">
+        <article class="container">
+            <div class="content-wrapper">
+                <h6>Ofrecemos una amplia gama de productospara perros, gatos, aves, peces y más.</h6>
+                <h1>Descubre todo lo que necesitas para cuidar y consentir a tu mascota.</h1>
+                <?php
+                $shop_page = get_page_by_path('shop');
+                $shop_page_url = get_permalink($shop_page->ID);
+                echo '<a class="btn btn-light" href="' . esc_url($shop_page_url) . '">Ir a la tienda <i class="fa-solid fa-store"></i></a>';
+                ?>
+            </div>
+        </article>
+    </article>
+</section>
+
+<section class="staff-grid bg-light">
+    <article class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="section-title">
+                    <h2>Nuestro equipo</h2>
+                    <p>
+                        En Vetexoticos, estamos orgullosos de contar con un equipo dedicado y apasionado por el bienestar de tus mascotas.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="row team">
+            <div class="col-lg-4 mb-5">
+                <div class="content-wrapper">
+                    <div class="image-wrapper mb-3">
+                        <img width="100%" class="border-radius" src="<?php echo get_template_directory_uri(); ?>/assets/images/staff-1.jpg" alt="Integrante de Vetexoticos">
+                    </div>
+                    <h4>Carlos Maretti</h4>
+                    <p class="description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident et repellat libero, aut blanditiis deserunt.
+                    </p>
+                    <div class="social-icons">
+                        <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href="#"><i class="fa-brands fa-x-twitter"></i></a>
+                        <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 mb-5">
+                <div class="content-wrapper">
+                    <div class="image-wrapper mb-3">
+                        <img width="100%" class="border-radius" src="<?php echo get_template_directory_uri(); ?>/assets/images/staff-2.jpg" alt="Integrante de Vetexoticos">
+                    </div>
+                    <h4>Roberto Pelistri</h4>
+                    <p class="description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident et repellat libero, aut blanditiis deserunt.
+                    </p>
+                    <div class="social-icons">
+                        <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href="#"><i class="fa-brands fa-x-twitter"></i></a>
+                        <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 mb-5">
+                <div class="content-wrapper">
+                    <div class="image-wrapper mb-3">
+                        <img width="100%" class="border-radius" src="<?php echo get_template_directory_uri(); ?>/assets/images/staff-3.jpg" alt="Integrante de Vetexoticos">
+                    </div>
+                    <h4>Marcos Rojo</h4>
+                    <p class="description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident et repellat libero, aut blanditiis deserunt.
+                    </p>
+                    <div class="social-icons">
+                        <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                        <a href="#"><i class="fa-brands fa-x-twitter"></i></a>
+                        <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </article>
 </section>
 
 <?php get_footer(); ?>
