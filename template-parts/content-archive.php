eb659a52bd07cb27e3e74acf441d8e3ac90fc953
membrillo amargo <lopezrunco@gmail.com>
2024-06-11 08:59:31 +0530

    style: Edit styles in comment form and content-archive.php.

--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -1,51 +1,30 @@
 <div class="container">
-    <div class="post mb-5">
+    <div class="post my-5">
         <div class="media">
-            <img class="mr-3 img-fluid post-thumb d-none d-md-flex" src="<?php the_post_thumbnail_url('thumbnail'); ?>" alt="image">
+            <img width="200px" class="mr-3 img-fluid post-thumb d-none d-md-flex border-radius" src="<?php the_post_thumbnail_url('thumbnail'); ?>" alt="image">
             <div class="media-body">
                 <h3 class="title mb-1">
-                    <a href="
-                        <?php
-                        the_permalink();
-                        ?>">
-                        <?php
-                        the_title();
-                        ?>
+                    <a href="<?php the_permalink(); ?>">
+                        <?php the_title(); ?>
                     </a>
                 </h3>
                 <div class="meta mb-1">
                     <span class="date">
-
-                        <?php
-                        the_date();
-                        ?>
-
+                        <?php the_date(); ?>
                     </span>
                     <span class="comment">
                         <a href="#">
-
-                            <?php
-                            comments_number();
-                            ?>
-
+                            <?php comments_number(); ?>
                         </a>
                     </span>
                 </div>
                 <div class="intro">
-
-                    <?php
-                    the_excerpt();
-                    ?>
-
+                    <?php the_excerpt(); ?>
                 </div>
                 <a class="more-link" href="
-                <?php
-                the_permalink();
-                ?>
-                ">Read more &rarr;</a>
+                    <?php the_permalink(); ?>
+                ">Ver más &rarr;</a>
             </div>
         </div>
     </div>
-
-
 </div>
