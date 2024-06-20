68649954698cfcfd3a92d538dec4516c3dafb809
viejo bucanero <lopezrunco@gmail.com>
2024-06-20 11:42:41 +0530

    refactor: Edit layout of 404.php page.

--- 404.php
+++ 404.php
@@ -2,28 +2,31 @@
 get_header();
 ?>
 
-<article>
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <h1 class="text-center mb-5">Page not found</h1>
+<section>
+    <article class="container">
+        <div class="row text-center">
+            <div class="col-lg-6">
+                <h1 class="text-center not-found-title">404!</h1>
+                <h2 class="text-center mb-5">Página no encontrada</h2>
+                <a href="<?php echo esc_url(home_url('/')); ?>" class="btn btn-primary mx-auto"><i class="fa-solid fa-house"></i> Volver a inicio</a>
             </div>
             <div class="col-lg-6">
-                <p>The page you are looking for has been moved or deleted.</p>
-                <p>Please, return to the home page or search for something in the search box.</p>
-
-                <a href="index.php" class="btn btn-primary">Back to Home page</a>
-            </div>
-            <div class="col-lg-6">
-
-                <?php
-                get_search_form();
-                ?>
-
+                <div class="mb-5">
+                    <p>
+                        La página que estás buscando ha sido movida o eliminada.
+                    </p>
+                    <p>
+                        Es posible que haya sido reubicada o que ya no esté disponible en este sitio web.
+                    </p>
+                    <p>
+                        Por favor, regresa a la página de inicio para explorar más contenido interesante, o utiliza la caja de búsqueda para encontrar información específica que estés buscando.
+                    </p>
+                </div>
+                <?php get_search_form(); ?>
             </div>
         </div>
-    </div>
-</article>
+    </article>
+</section>
 
 <?php
 get_footer();
