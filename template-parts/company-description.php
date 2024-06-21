5ab37b3a2aa72ee6bfa7de9ef3f287ae2545babb
viejo bucanero <lopezrunco@gmail.com>
2024-06-21 21:37:52 +0530

    chore: Update about us info.

--- template-parts/company-description.php
+++ template-parts/company-description.php
@@ -2,31 +2,37 @@
     <article class="container">
         <div class="row">
             <div class="col-lg-4 mb-4">
-                <img width="100%" class="border-radius" src="https://images.pexels.com/photos/220938/pexels-photo-220938.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Imagen de nosotros">
+                <img 
+                    width="100%" 
+                    class="border-radius mb-4" 
+                    src="<?php echo get_template_directory_uri()?> /assets/images/vetexoticos-nosotros.jpg" alt="Imagen de nosotros" 
+                />
+                <h5 class="mb-4">
+                    Somos una clínica dedicada exclusivamente a la atención médica y quirúrgica de animales de compañía no tradicionales.
+                </h5>
+                <img width="200" src="<?php echo get_template_directory_uri(); ?>/assets/images/signature.jpg" alt="Firma" />
             </div>
             <div class="col-lg-8">
                 <div class="row">
-                    <div class="col-12 mb-4">
-                        <h2>Nos apasiona cuidar de tus animales.</h2>
-                    </div>
                     <div class="col-lg-6">
+                        <h4>¿Por qué elegirnos?</h4>
                         <p>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti, facilis! Pariatur perferendis amet nesciunt alias debitis reprehenderit, ea excepturi nulla quibusdam libero. Enim culpa magni odio amet optio, velit ut. Pariatur perferendis amet nesciunt alias debitis reprehenderit, ea excepturi nulla quibusdam libero. Enim culpa magni odio amet optio, velit ut.
+                            En vetexoticos.uy nos hemos preparado para brindar una atención integral y especializada.
                         </p>
                         <p>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti, facilis! Pariatur perferendis amet nesciunt alias debitis reprehenderit, ea excepturi nulla quibusdam libero. Enim culpa magni odio amet optio, velit ut. Pariatur perferendis amet nesciunt alias debitis reprehenderit, ea excepturi nulla quibusdam libero.
+                            Somos un equipo joven y altamente capacitado para resolver las necesidades de nuestros pequeños pacientes.
                         </p>
-                    </div>
-                    <div class="col-lg-6">
+                        <h4>Nuestra filosofía</h4>
                         <p>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti, facilis! Pariatur perferendis amet nesciunt alias debitis reprehenderit, ea excepturi nulla quibusdam libero.
-                        </p>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti, facilis! Pariatur perferendis amet nesciunt alias debitis reprehenderit, ea excepturi nulla quibusdam libero. Enim culpa magni odio amet optio, velit ut. Enim culpa magni odio amet optio, velit ut. Pariatur perferendis amet nesciunt alias debitis reprehenderit, ea excepturi nulla quibusdam libero. Enim culpa magni odio amet optio, velit ut.
+                            Confiamos en la formación permanente, sumada al respeto y cariño por nuestros pacientes.
                         </p>
                     </div>
                     <div class="col-lg-6">
-                        <img width="200" src="<?php echo get_template_directory_uri(); ?>/assets/images/signature.jpg" alt="Firma">
+                        <h4>Equipo</h4>
+                        <p>
+                            Vetexoticos.uy fue fundado en 2019 por la Dra. Carina Esteves, quien desde pequeña tuvo interés por los animales y día a día sigue formándose con el fin de mejorar la medicina veterinaria de animales no tradicionales en nuestro país. Actualmente somos un equipo interdisciplinario unido con el fin de promover la salud de nuestros pacientes, brindando no sólo atención clínica sino también diagnósticos paraclínicos y atención quirúrgica en un mismo lugar.
+                        </p>
+
                     </div>
                 </div>
             </div>
