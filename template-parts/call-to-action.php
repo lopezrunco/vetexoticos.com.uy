bcf34b39bda16dfdb7c601e7048ed2ea86e6f7e0
viejo bucanero <lopezrunco@gmail.com>
2024-06-21 16:32:49 +0530

    core: Remove comments from call-to-action.php

--- template-parts/call-to-action.php
+++ template-parts/call-to-action.php
@@ -12,14 +12,3 @@
         </div>
     </article>
 </section>
-
-<!-- Usage: -->
-<!-- // Call to action section variables
-$cta_subtitle = 'Ofrecemos una amplia gama de productospara perros, gatos, aves, peces y más.';
-$cta_title = 'Descubre todo lo que necesitas para cuidar y consentir a tu mascota.';
-$shop_page = get_page_by_path('shop');
-$cta_button_url = get_permalink($shop_page->ID);
-$cta_button_text = 'Ir a la tienda';
-$cta_icon = 'fa-store';
-// Include the call-to-action template part with variables
-include get_template_directory() . '/template-parts/call-to-action.php'; -->
