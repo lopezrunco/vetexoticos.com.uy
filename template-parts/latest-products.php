48e68df0dca5f9682e6900732767dc57bff24109
viejo bucanero <lopezrunco@gmail.com>
2024-07-02 12:45:25 +0530

    refactor: Edit latest products template part to be a slider in mobile versions.

--- template-parts/latest-products.php
+++ template-parts/latest-products.php
@@ -11,19 +11,19 @@
 		if ($latest_products) {
 			$shop_page_url = get_permalink(wc_get_page_id('shop'));
 			echo '
-				<div class="section-title-more">
-					<div>
-						<span class="top-title">Cosas que ellos amarán</span>
-						<h2>Últimos productos</h2>
-					</div>
-					<div>
-						<a class="btn btn-primary" href="' . esc_url($shop_page_url) . '">Ver más productos <i class="fa-solid fa-chevron-right"></i></a>
-					</div>
-				</div>
-			';
-			echo '<div class="row">';
+                <div class="section-title-more">
+                    <div>
+                        <span class="top-title">Cosas que ellos amarán</span>
+                        <h2>Últimos productos</h2>
+                    </div>
+                    <div>
+                        <a class="btn btn-primary d-none d-lg-inline-block" href="' . esc_url($shop_page_url) . '">Ver más productos <i class="fa-solid fa-chevron-right"></i></a>
+                    </div>
+                </div>
+            ';
+			echo '<div class="row d-none d-lg-flex">'; // Desktop grid layout
 			foreach ($latest_products as $product) {
-				?>
+		?>
 				<div class="col-sm-6 col-lg-4 col-xl-3 mb-4">
 					<div class="product-wrapper">
 						<a href="<?php echo esc_url($product->get_permalink()); ?>">
@@ -33,8 +33,37 @@
 						</a>
 					</div>
 				</div>
-				<?php
+			<?php
 			}
+			echo '</div>';
+
+			// Mobile version (Bootstrap Carousel)
+			echo '<div class="row d-block d-lg-none">';
+			?>
+			<div id="latestProductsCarouselMobile" class="carousel slide" data-ride="carousel">
+				<div class="carousel-inner py-5">
+					<?php foreach ($latest_products as $index => $product) : ?>
+						<div class="carousel-item <?php echo $index === 0 ? 'active' : ''; ?>">
+							<div class="col-12">
+								<div class="product-wrapper">
+									<a href="<?php echo esc_url($product->get_permalink()); ?>">
+										<?php echo $product->get_image(); ?>
+										<h5><?php echo $product->get_name(); ?></h5>
+										<span class="price"><?php echo $product->get_price_html(); ?></span>
+									</a>
+								</div>
+							</div>
+						</div>
+					<?php endforeach; ?>
+				</div>
+				<a class="carousel-control-prev" href="#latestProductsCarouselMobile" role="button" data-slide="prev">
+					<i class="fa-solid fa-chevron-left position-arrow"></i>
+				</a>
+				<a class="carousel-control-next" href="#latestProductsCarouselMobile" role="button" data-slide="next">
+					<i class="fa-solid fa-chevron-right position-arrow"></i>
+				</a>
+			</div>
+		<?php
 			echo '</div>';
 		} else {
 			echo '<p>No se encontraron productos.</p>';
