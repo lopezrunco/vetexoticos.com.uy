b21972f8aeee218e1e44eb7984b486165837812b
viejo bucanero <lopezrunco@gmail.com>
2024-08-29 15:54:34 +0530

    feat: Add wapp icon in stock button.

--- template-parts/no-stock.php
+++ template-parts/no-stock.php
@@ -2,6 +2,6 @@
     href="https://api.whatsapp.com/send/?phone=59891210125&text=Hola%2C+quisiera+consultar+sobre+el+producto+<?php echo urldecode($product->get_title()) ?>&type=phone_number&app_absent=0"
     target="_blank">
         <span class="no-stock">
-            Consulte stock
+            <i class="fa-brands fa-whatsapp me-2"></i> Consulte stock
         </span>
 </a>
--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -62,7 +62,7 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 			href="https://api.whatsapp.com/send/?phone=59891210125&text=Hola%2C+quisiera+consultar+sobre+el+producto+' . $product->get_title() . '&type=phone_number&app_absent=0"
 			target="_blank">
 				<span class="no-stock">
-					Consulte stock
+					<i class="fa-brands fa-whatsapp me-2"></i> Consulte stock
 				</span>
 			</a>
 		';
